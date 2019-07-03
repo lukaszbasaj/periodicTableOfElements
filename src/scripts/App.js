27643addbbe3c5f32a45d482562ab432c6642a1f
Łukasz Basaj <lbasaj@gmail.com>
2019-07-03 22:50:58 +0200

    add abbr tags

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -7,10 +7,15 @@ table.forEach(renderPeriodicTable);
 
 function renderPeriodicTable(element) {
     let li = document.createElement('li');
+    let abbr = document.createElement('abbr');
+    abbr.setAttribute('title', `${element.name}`);
+    abbr.innerHTML = `${element.symbol}`;
     let elName = `${element.name}`;
     let atomicMass = Math.floor(`${element.atomic_mass}`);
     li.setAttribute('class', `${element.name}`);
     ul.appendChild(li);
-    li.innerHTML =`${li.innerHTML}${elName}${` `}${atomicMass}`;
+    li.appendChild(abbr);
+    li.innerHTML =`${li.innerHTML}${` `}${elName}${` `}${atomicMass}`;
+    
 }
 
