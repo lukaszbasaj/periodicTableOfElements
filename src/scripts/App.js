d0bc4b769469652abb4f359aa45b11eb4d640f39
Łukasz Basaj <lbasaj@gmail.com>
2019-07-03 22:37:46 +0200

    add App.js and data structure

---
+++ src/scripts/App.js
@@ -0,0 +1,16 @@
+import { table } from '../assets/data.js';
+    
+const ul = document.createElement('ol');
+ul.setAttribute('class','periodic-table');
+document.querySelector('main').appendChild(ul);
+table.forEach(renderPeriodicTable);
+
+function renderPeriodicTable(element) {
+    let li = document.createElement('li');
+    let elName = `${element.name}`;
+    let atomicMass = Math.floor(`${element.atomic_mass}`);
+    li.setAttribute('class', `${element.name}`);
+    ul.appendChild(li);
+    li.innerHTML =`${li.innerHTML}${elName}${` `}${atomicMass}`;
+}
+
