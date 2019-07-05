e79126e092096b2e0361a85ddde97b253009cea0
Łukasz Basaj <lbasaj@gmail.com>
2019-07-05 16:55:20 +0200

    adjust App.js to use semantic html tags while DOM is create

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,23 +1,30 @@
 import { table } from '../assets/data.js';
-    
-const ul = document.createElement('ol');
-ul.setAttribute('class','periodic-table');
-document.querySelector('main').appendChild(ul);
-table.forEach(renderPeriodicTable);
 
-function renderPeriodicTable(element) {
-    const li = document.createElement('li');
-    const abbr = document.createElement('abbr');
-    const sub = document.createElement('sub');
-    abbr.setAttribute('title', `${element.name}`);
-    abbr.innerHTML = `${element.symbol}`;
-    const elName = `${element.name}`;
-    const atomicMass = Math.floor(`${element.atomic_mass}`, -1);
-    const subValue = `${atomicMass}`;
-    li.setAttribute('class', `${element.name}${` `}${element.category}`);
-    ul.appendChild(li);
-    li.appendChild(abbr);
-    li.innerHTML =`${li.innerHTML}${` `}${elName}`;
-    li.appendChild(sub).innerHTML = `${subValue}`;
-}
+(function() {
+        const ol = document.createElement('ol');
+        ol.setAttribute('class', 'periodic-table');
+        document.querySelector('main').appendChild(ol);
+        table.forEach(renderPeriodicTable);
 
+        function renderPeriodicTable(element) {
+            const li = document.createElement('li');
+            const dl = document.createElement('dl');
+            const dt = document.createElement('dt');
+            const dd = document.createElement('dd');
+            const abbr = document.createElement('abbr');
+            const sub = document.createElement('sub');
+            const atomicMass = Math.floor(`${element.atomic_mass}`, -1);
+            const subValue = `${atomicMass}`;
+            const elName = `${li.innerHTML}${element.name}`;
+            abbr.setAttribute('title', `${element.name}`);
+            abbr.innerHTML = `${element.symbol}`;
+            li.setAttribute('class', `element ${element.name.toLowerCase()}${` `}${element.phase.toLowerCase()}${` `}${element.category}`);
+        ol.appendChild(li);
+        li.appendChild(dl);
+        dl.appendChild(dt);
+        dt.appendChild(abbr);
+        abbr.innerHTML = elName;
+        dl.appendChild(dd).innerHTML = `${element.summary}`;
+        dl.appendChild(sub).innerHTML = `${subValue}`;
+    }
+})();
